907a7aa5572e70f821f4e189f9e9c9c700d55f20
subakaran <subakaranmca@gmail.com>
2022-03-16 20:47:49 +0800

    test script is added

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -2,3 +2,4 @@
 php /var/www/html/secretlab/artisan migrate --force
 php /var/www/html/secretlab/artisan config:cache
 php /var/www/html/secretlab/artisan config:clear
+php /var/www/html/secretlab/artisan test
