7e399a32a62a72591b022f42c775bef29b31aba3
subakaran <subakaranmca@gmail.com>
2022-03-17 00:07:49 +0800

    Test command is updated

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -2,4 +2,5 @@
 php /var/www/html/secretlab/artisan migrate --force
 php /var/www/html/secretlab/artisan config:cache
 php /var/www/html/secretlab/artisan config:clear
-php /var/www/html/secretlab/artisan test --testsuite=Feature
+#php /var/www/html/secretlab/artisan test --testsuite=Feature
+vendor/bin/phpunit
