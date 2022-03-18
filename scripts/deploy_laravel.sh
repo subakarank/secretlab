a48605a27549f2625f4419c1ebd741613b6fb606
subakaran <subakaranmca@gmail.com>
2022-03-18 17:03:21 +0800

    Remove route cache from the deployment script

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 php /var/www/html/secretlab/artisan migrate --force
-php /var/www/html/secretlab/artisan config:clear
 php /var/www/html/secretlab/artisan config:cache
-php /var/www/html/secretlab/artisan route:cache
+php /var/www/html/secretlab/artisan config:clear
 cd /var/www/html/secretlab
 ./vendor/bin/phpunit
