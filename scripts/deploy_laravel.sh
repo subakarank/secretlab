4ec03b8c60f9e11edad44179c79a03c1533520e3
subakaran <subakaranmca@gmail.com>
2022-03-18 16:57:37 +0800

    Route cache is added for deployment script

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 php /var/www/html/secretlab/artisan migrate --force
-php /var/www/html/secretlab/artisan config:cache
 php /var/www/html/secretlab/artisan config:clear
+php /var/www/html/secretlab/artisan config:cache
+php /var/www/html/secretlab/artisan route:cache
 cd /var/www/html/secretlab
 ./vendor/bin/phpunit
